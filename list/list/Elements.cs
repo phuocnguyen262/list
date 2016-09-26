74e3af529437c7d158ae017420fc3c5817d2adce
phuocnguyen262 <https://github.com/phuocnguyen262/list.git>
2016-09-26 09:52:59 +0700

    Element

---
+++ list/list/Elements.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace list
+{
+    class Elements<T>
+    {
+        private T data;
+
+        public T Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+
+        Elements<T> next;
+
+        internal Elements<T> Next
+        {
+            get { return next; }
+            set { next = value; }
+        }
+    }
+}
